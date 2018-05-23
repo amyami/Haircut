5b47448b246c9b813abec540cdd2c0ea35ad0696
catherine <799313051@qq.com>
2018-05-23 21:13:11 +0800

    first commit

--- haircut/models.py
+++ haircut/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Appointment(models.Model):
+     time = models.CharField(max_length=180, null=True, blank=True)
+     customer_name = models.CharField(max_length=180, null=True, blank=True)
+     barber_name = models.CharField(max_length=180, null=True, blank=True)
--- haircut/views.py
+++ haircut/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render 
+from django.shortcuts import render
+from . models import Appointment
 
 
-def home_page(request): 
-
+def home_page(request):
     if request.method == 'POST':
-        time = request.POST.get('time')   
-        customer_name=request.POST.get("customer_name") 
-    
-        return render(request, 'haircut/home_page.html', {'time': time, 'customer_name': customer_name})  
-    
-    return render(request, 'haircut/home_page.html')          
+        time = request.POST.get('time')
+        customer_name = request.POST.get("customer_name")
+        barber_name = request.POST.get("barber_name")
+        comment_obj = Appointment.objects.create(
+            time=time,
+            customer_name=customer_name,
+            barber_name=barber_name
+        )
+        all_appointments = Appointment.objects.all()
+        context = {'appointments': all_appointments}
+
+        return render(request, 'haircut/home_page.html', context)
+    if request.method == "GET":
+        appointments = Appointment.objects.all()
+        context = {'appointments': appointments}   
+        return render(request, 'haircut/home_page.html', context)
